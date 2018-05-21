f61ea06b0cbb0b8ec8e16afa7194df55b7fc5725
ypwang1024 <ypwang1024@163.com>
2018-05-21 23:39:23 +0800

    代码提交180521 商品管理模块 空指针异常待处理

---
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -0,0 +1,49 @@
+package com.mmall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.service.IProductService;
+import com.mmall.vo.ProductDetailVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: mmall
+ * @description:
+ * @author: ypwang
+ * @create: 2018-05-20 21:00
+ **/
+@Controller()
+@RequestMapping("/product/")
+public class ProductController {
+
+    @Autowired
+    private IProductService iProductService;
+
+    /**
+     * 前端获取商品信息
+     *
+     * @param productId
+     * @return
+     */
+    @RequestMapping("product_detail.do")
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> productDtail(Integer productId) {
+        return iProductService.getProductDetail(productId);
+    }
+
+    @RequestMapping("product_list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> productList(@RequestParam(value = "keyword", required = false) String keyword,
+                                                @RequestParam(value = "categoryId", required = false) Integer categoryId,
+                                                @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                                @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                                @RequestParam(value = "orderBy", defaultValue = "") String orderBy) {
+        return iProductService.getProductByKeyWordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+
+}
