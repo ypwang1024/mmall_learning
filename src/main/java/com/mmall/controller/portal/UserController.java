3af5485437aded1ffe146bcb4417a2f95d1038fb
ypwang1024 <ypwang1024@163.com>
2019-04-18 21:21:14 +0800

    添加cookieutil工具类

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -5,6 +5,7 @@ import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -43,11 +46,14 @@ public class UserController {
      * 及其application/json;charset=UTF-8 将会自动序列化JSON
      * */
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse, HttpServletRequest httpServletRequest) {
         ServerResponse<User> response = iUserService.login(username, password);
 
         if (response.isSuccess()) {
             // session.setAttribute(ConstValue.CURRENT_USER, response.getData());
+            CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+            CookieUtil.readLoginToken(httpServletRequest);
+            CookieUtil.delLoginToken(httpServletRequest, httpServletResponse);
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()), ConstValue.RedisCacheExTime.REDIS_SESSION_EXTIME);
         }
         return response;
---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,86 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @program: mmall
+ * @description: Cookie 工具类
+ * @author: ypwang
+ * @create: 2019-04-18 20:57
+ **/
+@Slf4j
+public class CookieUtil {
+
+    /**
+     * 域名
+     */
+    private final static String COOKIE_DOMAIN = ".ypwang.com";
+
+    /**
+     * cookie name
+     */
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    /**
+     * 添加cookie
+     * @param response response
+     * @param token token
+     */
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        cookie.setPath("/");
+
+        // 单位是秒，如果这个maxage不设置的话，cookie就不会写入硬盘，而是写在内存，只在当前页面有效
+        // -1 表示永久，60 * 60 * 24 * 365 表示一年
+        cookie.setMaxAge(60 * 60 * 24 * 365);
+        log.info("write cookiename:{} ,cookievalue:{}", cookie.getName(), cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 读取cookie
+     * @param request request
+     * @return
+     */
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                log.info("read cookiename:{} ,cookievalue:{}", cookie.getName(), cookie.getValue());
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)) {
+                    log.info("return cookiename:{} ,cookievalue:{}", cookie.getName(), cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 删除cookie
+     * @param request request
+     * @param response response
+     */
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)) {
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    // 设置成0，代表删除此cookie
+                    cookie.setMaxAge(0);
+                    log.info("del cookiename:{} ,cookievalue:{}", cookie.getName(), cookie.getValue());
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
+    }
+}
